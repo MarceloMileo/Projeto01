90bd441940fc8ec45020b9c72f9377897b267769
Marcelo Mileo <marcelo.mileo@gmail.com>
2023-04-06 16:17:59 +0000

    [IMPLEMENTACAO] - Removido cargo da classe Funcionario

--- src/Modelo/Funcionario/Funcionario.php
+++ src/Modelo/Funcionario/Funcionario.php
@@ -6,20 +6,14 @@ use Projeto01\Banco\Modelo\{Pessoa, CPF};
 
 abstract class Funcionario extends Pessoa
 {
-    private $cargo;
     private $salario;
 
-    public function __construct(string $nome, CPF $cpf, string $cargo, float $salario)
+    public function __construct(string $nome, CPF $cpf, float $salario)
     {
         parent::__construct($nome, $cpf);
-        $this->cargo = $cargo;
         $this->salario = $salario;
     }
 
-    public function recuperaCargo(): string
-    {
-        return $this->cargo;
-    }
 
     public function alteraNome(string $nome): void
     {
